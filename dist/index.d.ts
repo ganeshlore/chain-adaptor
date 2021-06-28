d23a49adeff51e1414867147052820d610ccc933
Ganesh Lore <loreganesh@gmail.com>
2021-06-29 03:56:40 +0530

    Update Package

---
+++ dist/index.d.ts
@@ -0,0 +1,3 @@
+export declare function adaptor(network: any): Promise<unknown>;
+export declare function socket(network: any): Promise<unknown>;
+export declare function decrypt(salt: any, encoded: any): any;
